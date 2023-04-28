4ddd55e7865bc93d799e928648823c0caacc4054
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-28 22:53:45 +0300

    implemented My Queue

--- src/main/java/myCollections/TestingApp.java
+++ src/main/java/myCollections/TestingApp.java
@@ -2,17 +2,28 @@ package main.java.myCollections;
 
 public class TestingApp {
     public static void main(String[] args) {
-        System.out.println(" -----------------   testing My Array List -----------------------------");
-        MyArrayList<String> arrayList = new MyArrayList<>();
-        arrayList.add("hello!");
-        System.out.println("size - " + arrayList.getSize() + "; value - " + arrayList.get(0));
-        arrayList.add("worrld");
-        System.out.println("size - " + arrayList.getSize() + "; value - " + arrayList.get(1));
-        System.out.println(arrayList);
-        arrayList.remove(1);
-        System.out.println(arrayList);
-        arrayList.clear();
-        System.out.println(arrayList);
+//        System.out.println(" -----------------   testing My Array List -----------------------------");
+//        MyArrayList<String> arrayList = new MyArrayList<>();
+//        arrayList.add("hello!");
+//        System.out.println("size - " + arrayList.getSize() + "; value - " + arrayList.get(0));
+//        arrayList.add("worrld");
+//        System.out.println("size - " + arrayList.getSize() + "; value - " + arrayList.get(1));
+//        System.out.println(arrayList);
+//        arrayList.remove(1);
+//        System.out.println(arrayList);
+//        arrayList.clear();
+//        System.out.println(arrayList);
 
+        System.out.println(" -----------------   testing My Queue -----------------------------");
+        MyQueue<String> queue = new MyQueue<>();
+        queue.add("hello!");
+        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(0));
+        queue.add("worrld");
+        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(1));
+        System.out.println(queue);
+        queue.remove(1);
+        System.out.println(queue);
+        queue.clear();
+        System.out.println(queue);
     }
 }
