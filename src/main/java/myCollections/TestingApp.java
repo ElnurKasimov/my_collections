ac4bb5c6066074a2151a13722ef234a015fc1176
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-29 22:59:51 +0300

    implemented My Stack

--- src/main/java/myCollections/TestingApp.java
+++ src/main/java/myCollections/TestingApp.java
@@ -14,16 +14,31 @@ public class TestingApp {
 //        arrayList.clear();
 //        System.out.println(arrayList);
 
-        System.out.println(" -----------------   testing My Queue -----------------------------");
-        MyQueue<String> queue = new MyQueue<>();
-        queue.add("hello!");
-        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(0));
-        queue.add("worrld");
-        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(1));
-        System.out.println(queue);
-        queue.remove(1);
-        System.out.println(queue);
-        queue.clear();
-        System.out.println(queue);
+//        System.out.println(" -----------------   testing My Queue -----------------------------");
+//        MyQueue<String> queue = new MyQueue<>();
+//        queue.add("hello!");
+//        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(0));
+//        queue.add("worrld");
+//        System.out.println("size - " + queue.getSize() + "; value - " + queue.get(1));
+//        System.out.println(queue);
+//        queue.remove(1);
+//        System.out.println(queue);
+//        queue.clear();
+//        System.out.println(queue);
+
+
+        System.out.println(" -----------------   testing My Satck -----------------------------");
+        MyQueue<String> stack = new MyQueue<>();
+        stack.add("hello!");
+        System.out.println("size - " + stack.getSize() + "; value - " + stack.get(0));
+        stack.add("worrld");
+        System.out.println("size - " + stack.getSize() + "; value - " + stack.get(1));
+        System.out.println(stack);
+        stack.remove(1);
+        System.out.println(stack);
+        stack.clear();
+        System.out.println(stack);
+
+
     }
 }
