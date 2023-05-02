6acc1202d4650d3f04b04aba70dfd3ce1459d4c2
ElnurKasimov <ekasimov2012@gmail.com>
2023-05-02 22:15:21 +0300

    implemented MySinglyLinkedList but not checked if methods work correct or no

--- src/main/java/myCollections/MySinglyLinkedList.java
+++ src/main/java/myCollections/MySinglyLinkedList.java
@@ -1,4 +1,71 @@
 package main.java.myCollections;
 
+
 public class MySinglyLinkedList {
+    ListNode head;
+
+    public MySinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode current = head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+    }
+
+    public void delete(int val) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.val == val) {
+            head = head.next;
+            return;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+                return;
+            }
+
+            current = current.next;
+        }
+    }
+
+    public void printList() {
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+
+        System.out.println();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
 }
+
